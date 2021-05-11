49b42f9a0d04dd634fb52dd83e072cde5cdd19ee
codeawaywithme <82777534+codeawaywithme@users.noreply.github.com>
2021-05-10 22:41:11 -0500

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,13 @@
+//Navigation
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 const home = document.querySelector('.home');
-
+// Scroll Lock
+const html = document.querySelector("html");
+const body = document.querySelector("body");
 
 let showMenu = false;
 
@@ -17,7 +20,10 @@ function toggleMenu() {
     menuNav.classList.add('open');
     navItems.forEach(item => item.classList.add('open'));
     home.classList.add('open');
-    
+    //Scroll Lock
+    html.classList.add(".scrollLock");
+    body.classList.add(".scrollLock");
+    disableScrolling();
     showMenu = true;
   } else {
     hamburger.classList.remove('open');
@@ -25,8 +31,21 @@ function toggleMenu() {
     menuNav.classList.remove('open')
     navItems.forEach(item => item.classList.remove('open'));
     home.classList.remove('.open')
+    html.classList.remove(".scrollLock");
+    body.classList.remove(".scrollLock");
+    enableScrolling();
 
 
     showMenu = false; 
   }
 }
+function disableScrolling(){
+  var x=window.scrollX;
+  var y=window.scrollY;
+  window.onscroll=function(){window.scrollTo(x, y);};
+}
+function enableScrolling(){
+  window.onscroll=function(){};
+}
+
+
